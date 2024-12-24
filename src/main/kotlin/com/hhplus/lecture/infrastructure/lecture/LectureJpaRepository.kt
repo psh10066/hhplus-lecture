eb0c51ce576d7b1a55ae8218905d495c1ba2d072
이호준 <psh10066@gmail.com>
2024-12-25 00:26:31 +0900

    Fix: 특강 > 강연자 N + 1 이슈 대응

--- src/main/kotlin/com/hhplus/lecture/infrastructure/lecture/LectureJpaRepository.kt
+++ src/main/kotlin/com/hhplus/lecture/infrastructure/lecture/LectureJpaRepository.kt
@@ -1,15 +1,18 @@
 package com.hhplus.lecture.infrastructure.lecture
 
 import com.hhplus.lecture.domain.lecture.Lecture
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import java.time.LocalDateTime
 
 interface LectureJpaRepository : JpaRepository<Lecture, Long> {
 
+    @EntityGraph(attributePaths = ["lecturer"])
     fun findAllByStartTimeGreaterThanEqualAndStartTimeLessThan(
         startTimeIsGreaterThanEqual: LocalDateTime,
         startTimeIsLessThan: LocalDateTime
     ): List<Lecture>
 
+    @EntityGraph(attributePaths = ["lecturer"])
     fun findAllBySubscriptions_userId(userId: Long): List<Lecture>
 }
