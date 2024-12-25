337b7f6b6a63580dc47d586c412016ee92793cdd
이호준 <psh10066@gmail.com>
2024-12-26 00:47:00 +0900

    Feat: 최소한의 에러 메시지 추가

---
+++ src/main/kotlin/com/hhplus/lecture/api/controller/ApiControllerAdvice.kt
@@ -0,0 +1,26 @@
+package com.hhplus.lecture.api.controller
+
+import com.hhplus.lecture.support.response.ApiResponse
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class ApiControllerAdvice {
+    private val log: Logger = LoggerFactory.getLogger(javaClass)
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ApiResponse<Unit>> {
+        log.error("IllegalArgumentException : {}", e.message, e)
+        return ResponseEntity(ApiResponse.error(e.message), HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleException(e: Exception): ResponseEntity<ApiResponse<Unit>> {
+        log.error("Exception : {}", e.message, e)
+        return ResponseEntity(ApiResponse.error(e.message), HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+}
